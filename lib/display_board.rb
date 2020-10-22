f820cd242831840583cc9d1185ee5e8bd1b1ab57
Nina K <kazzim@icloud.com>
2020-10-22 01:18:00 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -2,11 +2,12 @@
 # out the current state.
 
 def display_board(board)
- puts "   |   |   "
+ puts "#{board[0]}|#{board[1]}|#{board[2]}"
  puts "-----------"
  puts "   |   |   "
  puts "-----------"
  puts "   |   |   "
 end
 
-board = [" ", " ", " ", " ", "X", " ", " ", " ", " "]
+board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+
